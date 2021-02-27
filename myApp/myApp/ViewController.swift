9d8f3348b98f1ab7161c4fb0b957d5f710507eeb
Ramin <ramin@Ramins-MacBook-Pro.local>
2021-02-27 21:46:10 +0100

    app created

---
+++ myApp/myApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Ramin on 27/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
